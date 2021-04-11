bb3d5b8ec151c5355796174f70d6388cb753ef70
Stan Chen <stanleychen587@gmail.com>
2021-04-11 09:49:34 -0600

    added processing scripts to put extracted files in a list for scrapping

--- ingest-and-process-secedgar.py
+++ ingest-and-process-secedgar.py
@@ -7,6 +7,8 @@
 # 
 # [python tool](https://sec-edgar.github.io/sec-edgar/)
 
+# # Ingest Data
+
 # In[8]:
 
 
@@ -24,23 +26,72 @@ from secedgar.filings import Filing, FilingType
 
 my_filings = Filing(cik_lookup=['gme'],
                     filing_type=FilingType.FILING_10K,
-                    count=1326380,
+                    count=1326380, # all avaliable gme reports
                    user_agent='deeptendies')
 
 my_filings.save('filings')
 
 
-# In[53]:
+# # Parse Data
+
+# In[56]:
 
 
 import glob
 from secedgar.parser import MetaParser
+from pathlib import Path
+
+out_dir='parsed_filings'
+Path(out_dir).mkdir(parents=True, exist_ok=True)
 
 directory='filings'
 for filepath in glob.iglob('filings/*/*/*.txt'):
     print(filepath)
     parser=MetaParser()
-    parser.process(infile=filepath, out_dir='parsed_filings', create_subdir=True, rm_infile = False)
+    parser.process(infile=filepath, out_dir=out_dir, create_subdir=True, rm_infile = False)
 
 
-# After parsing, the parsed data will be created at 
+# # Extract Data
+# extra data from a specific report type, and put them in a result list
+
+# After parsing, the parsed data will be created at `out_dir='parsed_filings'`
+# need to access the file like so:
+
+# In[80]:
+
+
+import json
+import os
+# os.walk(out_dir)
+results = []
+filings=[x[0] for x in os.walk(out_dir)]
+for i in filings:
+    meta=os.path.join(i, '*metadata.json')
+    for filepath in glob.iglob(meta):
+        print(filepath)
+        with open(filepath) as file:
+            data = json.load(file)
+            for doc in data['documents']:
+                if doc['type'] == '10-K':
+                    print (doc['filename'])
+                    filepaths2 = os.path.join(i, '*'+doc['filename'])
+                    for filepath2 in glob.iglob(filepaths2):
+                        with open(filepath2, 'r') as file2:
+                            print (file2)
+                            result = file2.read()
+                            results.append(result)
+
+
+# # What's left to do?
+# Scrape & mine data
+# 
+# with the above code, all the 10-k filing reports's html are stored in the result list, use beautiful soup & data scraping techniques to extra the business information you're interested in. 
+# 
+# resources:
+# https://realpython.com/beautiful-soup-web-scraper-python/
+
+# In[79]:
+
+
+result
+
